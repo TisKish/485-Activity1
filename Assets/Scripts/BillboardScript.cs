e5d5693c80cdb195a2f5c1fe7fb7cae65d569536
Thijs Thijssen <T.Thijssen2@student.han.nl>
2019-09-11 16:34:17 -0500

    Assets for combat with health bars and enemies

---
+++ Assets/Scripts/BillboardScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardScript : MonoBehaviour
+{
+    public GameObject myCamera;
+
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        transform.LookAt(transform.position + myCamera.transform.rotation * Vector3.forward,
+            myCamera.transform.rotation * Vector3.up);
+    }
+
+    void Init()
+    {
+        myCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+}
---
+++ Assets/Scripts/SingleEnemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleEnemy : Monster
+{
+    private void Start()
+    {
+        
+    }
+}
